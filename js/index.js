25a68107a5deccd29ac8546f691b288ed7775abd
Giovana <giamorim2101@gmail.com>
2024-09-03 09:20:59 -0300

    segunda aula do projeto

--- js/index.js
+++ js/index.js
@@ -3,7 +3,17 @@ const diaMesAno = document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
 
 const btnBaterPonto = document.getElementById("btn-bater-ponto");
-btnBaterPonto.addEventListener("click", register);
+const dialogPonto = document.getElementById("dialog-ponto");
+
+btnBaterPonto.addEventListener("click", function() {
+    dialogPonto.showModal();
+});
+
+
+const btnDialogFechar = document.getElementById("btn-dialog-fechar");
+btnDialogFechar.addEventListener("click", () => {
+    dialogPonto.close();
+});
 
 diaSemana.textContent = getWeekDay();
 diaMesAno.textContent = getCurrentDate();
@@ -16,21 +26,23 @@ function getWeekDay() {
 }
 
 function getCurrentDate() {
+    // Alterar a solução para considerar padStart ou slice
+    // Considerar formatos diferentes da data, conforme localização
+    // 
     const date = new Date();
     return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
 }
 
 function getCurrentTime() {
     const date = new Date();
-    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
 }
 
 function printCurrentHour() {
     horaMinSeg.textContent = getCurrentTime();
-}
-
-function register() {
-    alert("Ponto batido com sucesso!");
 }
 
 setInterval(printCurrentHour, 1000);
